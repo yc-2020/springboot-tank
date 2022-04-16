6de1cb772e01a0693eb046dfc2a7559e9e26ccb1
yc <yc@yuchao.xyz>
2022-04-16 10:15:49 +0800

    Initial commit

---
+++ client/src/main/java/com/example/tank/client/ClientApplication.java
@@ -0,0 +1,16 @@
+package com.example.tank.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yc
+ */
+@SpringBootApplication
+public class ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+    }
+
+}
